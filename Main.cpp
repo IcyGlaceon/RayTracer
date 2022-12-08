ca5333e3108a49c46e2479c80d7e91aad38d1c61
Carter Small <csmall@student.neumont.edu>
2022-12-08 08:28:59 -0700

    Plane

--- Main.cpp
+++ Main.cpp
@@ -2,19 +2,26 @@
 #include "Renderer/Renderer.h"
 #include "Object/Sphere.h"
 #include "Object/Scene.h"
+#include "Object/Plane.h"
 
 int main(int, char**)
 {
+    const int width = 800;
+    const int height = 600;
+    const int samples = 10;
+
     Renderer renderer; 
     renderer.Initialize(); 
-    renderer.CreateWindow(1200, 600);
+    renderer.CreateWindow(width,height);
 
-    Canvas canvas(1200, 600, renderer);
-    Camera camera({ 0, 1, 2 }, { 0, 0, 0 }, { 0, 1, 0 }, 70.0f, 600 / (float)300);
+    Canvas canvas(width, height, renderer);
+    Camera camera({ 0, 1, 2 }, { 0, 0, 0 }, { 0, 1, 0 }, 70.0f, width / (float)height);
     Scene scene;
 
     scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 0, 0, -1 }, 0.5f, std::make_unique<Lambertian>(color3{ 0,1,0 })));
-    scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 0, -100.5f, -1 }, 100.0f, std::make_unique<Lambertian>(color3{ 0,0,0.5 })));
+    scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 1, 0, -1 }, 0.5f, std::make_unique<Metal>(color3{ 1,1,1 }, 0.1f)));
+   // scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 0, -100.5f, -1 }, 100.0f, std::make_unique<Lambertian>(color3{ 0,0,0.5 })));
+    scene.AddObject(std::make_unique<Plane>(glm::vec3{ 0, -0.5, 0 }, glm::vec3{ 0, 1, 0 }, std::make_unique<Lambertian>(color3{ 0.2f, 0.2f, 0.2f })));
 
     bool quit = false; 
     while (!quit) 
@@ -39,12 +46,13 @@ int main(int, char**)
 
         canvas.Clear({0,0,0,1});
 
-        renderer.Render(canvas, scene, camera);
+        renderer.Render(canvas, scene, camera, samples);
         canvas.Update();
 
         renderer.CopyCanvas(canvas);
         renderer.Present();
     }
-    renderer.Shutdown(); 
+    renderer.Shutdown();
+
     return 0;
 }
--- Renderer/Renderer.cpp
+++ Renderer/Renderer.cpp
@@ -50,14 +50,15 @@ void Renderer::Present()
     SDL_RenderPresent(m_renderer);
 }
 
-void Renderer::Render(Canvas& canvas, Scene& scene, Camera& camera)
+void Renderer::Render(Canvas& canvas, Scene& scene, Camera& camera, int samples)
 {
+    
     // camera / viewport 
     glm::vec3 lowerLeft{ -2, -1, -1 };
     glm::vec3 eye{ 0, 0, 0 };
     glm::vec3 right{ 4, 0, 0 };
     glm::vec3 up{ 0, 2, 0 };
-
+/*
     for (int y = 0; y < canvas.GetHeight(); y++)
     {
         for (int x = 0; x < canvas.GetWidth(); x++)
@@ -80,27 +81,37 @@ void Renderer::Render(Canvas& canvas, Scene& scene, Camera& camera)
             //canvas.DrawPoint({ x, y }, color4(color, 1));
         }
     }
-
-    for (int y = 0; y < canvas.m_height; y++)
+    */
     {
-        for (int x = 0; x < canvas.m_width; x++)
+        for (int y = 0; y < canvas.m_height; y++)
         {
-            // get normalized (0 - 1) u, v coordinates from screnn x and y 
+            for (int x = 0; x < canvas.m_width; x++)
+            {
+                color3 color{ 0, 0, 0 };
+                for (int s = 0; s < samples; s++)
+                {
 
-            glm::vec2 point = glm::vec2{ x, y } / glm::vec2{ canvas.m_width, canvas.m_height };
-            // flip y 
-            point.y = 1.0f - point.y;
+                    // get normalized (0 - 1) u, v coordinates from screen x and y 
+                   // add random value (0-1) to screen x and y for anti-aliasing  
+                    glm::vec2 point = glm::vec2{ random01() + x, random01() + y } / glm::vec2{ canvas.m_width, canvas.m_height };
 
-            // create ray from camera 
-            Ray ray = camera.PointToRay(point);
+                    // flip y 
+                    point.y = 1.0f - point.y;
 
-            // cast ray into scene, get color 
-            RaycastHit raycastHit;
-            color3 color = scene.Trace(ray, 0.001f, 1000.0f, raycastHit, 5);
-            canvas.DrawPoint({ x, y }, color4(color, 1));
+                    // create ray from camera 
+                    Ray ray = camera.PointToRay(point);
+
+                    // cast ray into scene 
+                    RaycastHit raycastHit;
+                    // add trace color value to color 
+                    color = color + scene.Trace(ray, 0.001f, 1000.0f, raycastHit, 5);
+
+                }
+                color = color / glm::vec3{ samples,samples,samples };
+                canvas.DrawPoint({ x, y }, color4(color, 1));
+            }
         }
     }
-
 }
 
 color3 Renderer::GetBackgroundFromRay(const Ray& ray)
